755fdddd943cfb6c56bc26dd60aae45e3dc0aaf0
Gulyaev <egulyaev1@yandex.ru>
2021-09-27 11:24:30 +0300

    Fix SQL requests

--- billing/main/views.py
+++ billing/main/views.py
@@ -1,7 +1,6 @@
 import csv
 
-from django.db.models import Count, Prefetch
-from django.db.models.functions import TruncDay
+from django.db.models import Count
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
@@ -52,7 +51,8 @@ def day_purchases_list(request):
     if request.method == 'POST':
         form = GetDateForm(request.POST)
         if form.is_valid():
-            purchases = Purchases.objects.select_related('product', 'promo').filter(buy_date=form.cleaned_data['purchase'])
+            purchases = Purchases.objects.select_related('product', 'promo').filter(
+                buy_date=form.cleaned_data['purchase'])
             writer = csv.writer(response)
             writer.writerow(
                 ['Дата и время покупки',
@@ -65,19 +65,19 @@ def day_purchases_list(request):
                 if purchase.promo:
                     writer.writerow(
                         [
-                        purchase.buy_time,
-                        purchase.product,
-                        purchase.product.price,
-                        purchase.promo.name,
-                        purchase.promo.percent
+                            purchase.buy_time,
+                            purchase.product,
+                            purchase.product.price,
+                            purchase.promo.name,
+                            purchase.promo.percent
                         ]
                     )
                 else:
                     writer.writerow(
-                            [purchase.buy_time,
-                             purchase.product,
-                             purchase.product.price, 0, 0]
-                        )
+                        [purchase.buy_time,
+                         purchase.product,
+                         purchase.product.price, 0, 0]
+                    )
             return response
     else:
         form = GetDateForm()
@@ -100,23 +100,21 @@ def get_promo_effect_report(request):
          'Среднее число продаваемых товаров в день без скидки']
     )
     promo_purchase = Promo.objects.all().exclude(category__isnull=True).select_related('category').annotate(
-        Count('promobuy__id', distinct=True),
-        Count('promobuy__buy_date', distinct=True),
-        Count('category__product__purchases__id', distinct=True)
+        cnt_promo_buys=Count('promobuy__id', distinct=True),
+        days_promo_buys=Count('promobuy__buy_date', distinct=True),
+        cnt_all_buys=Count('category__product__purchases__id', distinct=True)
     )
     for promo in promo_purchase:
-        cnt_promo_buys = promo.promobuy__id__count
-        days_promo_buys = promo.promobuy__buy_date__count
-        avg_promo_buys = cnt_promo_buys / days_promo_buys
-        cnt_not_promo_buys = promo.category__product__purchases__id__count - cnt_promo_buys
-        avg_not_promo_buys = cnt_not_promo_buys / days_promo_buys
+        avg_promo_buys = promo.cnt_promo_buys / promo.days_promo_buys
+        cnt_not_promo_buys = promo.cnt_all_buys - promo.cnt_promo_buys
+        avg_not_promo_buys = cnt_not_promo_buys / promo.days_promo_buys
         writer.writerow(
             [
-             promo.name,
-             promo.category,
-             avg_promo_buys,
-             avg_not_promo_buys
-             ]
+                promo.name,
+                promo.category,
+                avg_promo_buys,
+                avg_not_promo_buys
+            ]
         )
     return response
 
@@ -130,9 +128,9 @@ def get_promo_effect_report_product(request):
                                         'filename="promo_effect_product.csv"'},
     )
     promos = Promo.objects.all().exclude(product__isnull=True).select_related('product').annotate(
-        Count('promobuy__id', distinct=True),
-        Count('promobuy__buy_date', distinct=True),
-        Count('product__purchases__id', distinct=True)
+        cnt_promo_buys=Count('promobuy__id', distinct=True),
+        days_promo_buys=Count('promobuy__buy_date', distinct=True),
+        cnt_all_buys=Count('product__purchases__id', distinct=True)
     )
     writer = csv.writer(response)
     writer.writerow(
@@ -142,11 +140,9 @@ def get_promo_effect_report_product(request):
          'Среднее число продаваемых товаров в день без скидки']
     )
     for promo in promos:
-        cnt_promo_buys = promo.promobuy__id__count
-        days_promo_buys = promo.promobuy__buy_date__count
-        avg_promo_buys = cnt_promo_buys / days_promo_buys
-        cnt_not_promo_buys = promo.product__purchases__id__count - cnt_promo_buys
-        avg_not_promo_buys = cnt_not_promo_buys / days_promo_buys
+        avg_promo_buys = promo.cnt_promo_buys / promo.days_promo_buys
+        cnt_not_promo_buys = promo.cnt_all_buys - promo.cnt_promo_buys
+        avg_not_promo_buys = cnt_not_promo_buys / promo.days_promo_buys
         writer.writerow(
             [
                 promo.name,
